76926a51648b754ef2b27a5ee240b4fd1171ee06
jlcolon-22 <jlcolon368@gmail.com>
2024-06-02 19:48:58 +0800

    first

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+import preset from './vendor/filament/support/tailwind.config.preset'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [preset],
+  content: [ "./resources/**/*.blade.php",
+  "./resources/**/*.js",
+  "./resources/**/*.vue",
+  './app/Filament/**/*.php',
+  './resources/views/filament/**/*.blade.php',
+  './vendor/filament/**/*.blade.php',],
+  theme: {
+    extend: {
+        colors:{
+            pup: '#791417'
+        }
+    },
+  },
+  plugins: [    require('@tailwindcss/typography'),
+],
+}
+
